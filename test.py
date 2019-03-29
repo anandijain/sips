ab38738418d841e163d994ee0aaa4b7c4a6aa4e7
Anand <anandj@uchicago.edu>
2019-03-29 16:18:17 -0700

    going to train on scores

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
-import lines_sip.lines as ll
+import lines as ll
 import matplotlib.pyplot as plt 
 import threading
 import time
 
-class Test:
+class Scraper:
     def __init__(self, file_name, header=0, game_type=1, run=1):
         self.fn = file_name
         self.gt = game_type
@@ -13,22 +13,16 @@ class Test:
         print('num_games: ' + str(len(self.sip.games)))
         if len(self.sip.games) > 0:
             self.game = self.sip.games[0]
-        if run != 0:
+
+        if run:
             self.run()
-        # self.game.info()
 
     def run(self):
-        self.sip.run()
+        while True:
+            self.sip.step()
+            # for game in self.sip.games:
+            #     action = sipqn.choose_action()
 
-
-# unit test
-
-# t = threading.Thread(target = Test, name = 'Sippy1',
-#                     args = ('nba2_test', 0, 1, 1))
-# t.start()
-
-
-# x = input()
-
-# print(x)
+    def game_states():
+        pass
 
