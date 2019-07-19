ea4dd92ab72ed6e95b5e49c5be3b6cc6e2211a90
anandijain <anandj@uchicago.edu>
2019-07-19 18:36:26 -0500

    odds urls

--- lvo/lvo.py
+++ lvo/lvo.py
@@ -3,28 +3,54 @@ import bs4
 
 '''
 Schema
-root -> each sport (global and lv)
-each sport -> # TODO each game
+1. get sport names
+self.sports = [string list]
+2. for each sport go to scoreboard
+self.root + sport + /scoreboard
 
-currently have code to get every link, but there is like 90% redundancy
+lines = mlbs.find_all('a', href=True, text='Line Movements')
+
 
 # todos
 store list of sports and extensions for url construction instead of bs4 retrieval
+currently have code to get every link, but there is like 90% redundancy
 
-
-
+example line url :
+http://www.vegasinsider.com/wnba/odds/global/line-movement/mystics-@-fever.cfm/date/07-19-19
+split('/')
 '''
+
 class LV:
     def __init__(self):
-        self.url = 'http://www.vegasinsider.com'
-        self.get_soup_urls()
+        self.sports = \
+            ['nfl', 'nba', 'nhl', 'mlb', 'college-football', 'college-basketball', 'auto-racing', 'golf'
+            'horse-racing', 'soccer', 'boxing', 'wnba', 'afl', 'poker', 'cfl']
 
-    def get_soup_urls(self):
-        p = r.get(self.url + '/odds/').text
-        self.soup = bs4.BeautifulSoup(p, 'html.parser')
-        gl_odds_urls = self.soup.find('img', {'alt' : 'Free Global Sportsbook Betting Odds and Lines'}).find_parent('tbody')
-        lv_odds_urls = self.soup.find('img', {'alt' : 'Free Las Vegas Sportsbook Betting Odds and Lines'}).find_parent('tbody')
-        self.odds_links = [link['href'] for link in gl_odds_urls.find_all('a') + lv_odds_urls.find_all('a')]
+        self.url = 'http://www.vegasinsider.com'
+        self.odds_urls = []  # list of urls
+        self.odds_tables = []
+
+        self.get_odds_urls()
+        self.get_odds_tables()
+
+    def get_odds_urls(self):
+        for sport in self.sports:
+            sb_url = self.url + '/' + sport + '/scoreboard/'
+            scoreboard = req_soup(sb_url)
+            lines = scoreboard.find_all('a', href=True, text='Line Movements')
+            if len(lines) == 0:
+                continue
+            self.odds_urls += [line['href'] for line in lines]
+
+        self.odds_urls += [global_from_lv(url) for url in self.odds_urls]
+        # gl_odds_urls = self.soup.find('img', {'alt' : 'Free Global Sportsbook Betting Odds and Lines'}).find_parent('tbody')
+        # lv_odds_urls = self.soup.find('img', {'alt' : 'Free Las Vegas Sportsbook Betting Odds and Lines'}).find_parent('tbody')
+        # self.odds_links = [link['href'] for link in gl_odds_urls.find_all('a') + lv_odds_urls.find_all('a')]
+
+    def get_odds_tables(self):
+        for url in self.odds_urls:
+            lines = req_soup(url)
+            self.odds_tables += lines.find_all('table', {'class' : 'rt_railbox_border2'})
 
     def test_sport(self):
         for odd_link in self.odds_links:
@@ -33,3 +59,19 @@ class LV:
             links = []
             for dt in data_tables:
                 links += [link['href'] for link in dt.find_all('a', {'class' : 'cellTextNorm'})]
+
+def req_soup(url):
+    page = r.get(url).text
+    soup = bs4.BeautifulSoup(page, 'html.parser')
+    return soup
+
+
+def global_from_lv(url):
+    global_url = ''
+    spl = url.split('/')
+    for item in spl:
+         if spl.index(item) == 5:
+                 item = 'global'
+         item += '/'
+         global_url += item
+    return global_url
