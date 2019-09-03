cd01cf393e09dc3899d4f6fb6200402fa7e8d9a5
anandijain <anandj@uchicago.edu>
2019-09-03 11:30:06 -0500

    dfs to timearrays

--- jl/sips.jl
+++ jl/sips.jl
@@ -1,7 +1,7 @@
 module Sips
 	using TimeSeries, CSV, Plots
 
-	function get_dfs
+	function get_dfs()
 		dfs = []
 		files = readdir("/home/sippycups/Downloads/datasets/price-volume-data-for-all-us-stocks-etfs/Data/Stocks")
 		for file in files
@@ -9,4 +9,17 @@ module Sips
 			push!(dfs, df)
 		end
 		return dfs
+
+	function dfs_to_ts(dfs)
+		time_series = []
+		for df in dfs:
+			date_times = df.Dates
+			ohlc_values = df[:, 2:5]
+			column_names = names(df)[2:5]
+			metadata = df[:, 6]
+			time_array = TimeArray(date_times, ohlc_values, column_names, metadata)
+			push!(time_series, time_array)
+		end
+		return time_series
 	end
+end
