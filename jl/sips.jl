45bac1fd184e760b01765ba61cfafb300430a4c6
anandijain <anandj@uchicago.edu>
2019-09-03 13:49:27 -0500

    couple of functions

--- jl/sips.jl
+++ jl/sips.jl
@@ -1,25 +1,43 @@
 module Sips
 	using TimeSeries, CSV, Plots
 
-	function get_dfs()
+	function get_dfs(directory::String)
 		dfs = []
-		files = readdir("/home/sippycups/Downloads/datasets/price-volume-data-for-all-us-stocks-etfs/Data/Stocks")
+		files = readdir(directory)
 		for file in files
-			df = CSV.read(file)
+			fp = string(directory, file)
+			df = CSV.read(fp)
 			push!(dfs, df)
 		end
 		return dfs
+	end
 
 	function dfs_to_ts(dfs)
 		time_series = []
-		for df in dfs:
-			date_times = df.Dates
-			ohlc_values = df[:, 2:5]
+		for df in dfs
+			if length(names(df)) == 0
+				continue
+			end
+
+			date_times = convert(Array, df.Date)
+			ohlc_values = convert(Matrix, df[:, 2:5])
 			column_names = names(df)[2:5]
 			metadata = df[:, 6]
 			time_array = TimeArray(date_times, ohlc_values, column_names, metadata)
 			push!(time_series, time_array)
 		end
 		return time_series
+	end
+
+	function lstm_tuples(df; window_len=10)
+		tups = []
+		for i in range(1, stop=size(df, 1) - window_len - 1)
+			idx = i + window_len
+			x = convert(Matrix, df[i:idx, :])
+			y = convert(Array, df[idx + 1, :])
+			tup = (x, y)
+			push!(tups, tup)
+		end
+		return tups
 	end
 end
