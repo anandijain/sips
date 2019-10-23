61e6b60073025ad85418c7a61acd04f6d05f10e2
anandijain <anandj@uchicago.edu>
2019-10-22 21:09:05 -0500

    mid way thru refactor

--- sips/lines/bov/bet_utils.py
+++ sips/lines/bov/bet_utils.py
@@ -4,7 +4,7 @@ import bs4
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-
+from sips.lines.bov import better
 ROOT_URL = 'https://www.bovada.lv/'
 GAME_CLASS = 'coupon-content'
 MARKETS_CLASS = 'markets-container'
@@ -30,13 +30,100 @@ MARKET_NAMES = {
 }
 
 
+def get_games(driver, verbose=False):
+    '''
+    uses selenium to find the 'coupon-content'(s)
+    each game has a tag_name of 'section'
+    '''
+
+    games = driver.find_elements_by_class_name(GAME_CLASS)
+
+    if verbose:
+        print(f'len(games): {len(games)}')
+        _ = [print(g.text) for g in games]
+
+    return games
+
+
+def mkts_from_game(game, verbose=False):
+
+    game_mkts = game.find_elements_by_class_name(u.MARKETS_CLASS)
+
+    if verbose:
+        _ = [print(mkt.text) for mkt in game_mkts]
+
+    return game_mkts
+
+
+def get_mkts(games, verbose=False):
+    '''
+    returns MARKETS_CLASS class name with selenium
+    '''
+    mkts = []
+
+    for game in games:
+        game_mkts = mkts_from_game(game, verbose=verbose)
+        mkts += game_mkts
+
+    if verbose:
+        print(f'len(mkts): {len(mkts)}')
+    return mkts
+
+
+def buttons_from_mkts(mkts, verbose=False):
+    '''
+    returns list of buttons for each market provided
+    '''
+    buttons = []
+    for mkt in mkts:
+        mkt_buttons = mkt.find_elements_by_class_name(u.BET_BUTTON_CLASS)
+        buttons += mkt_buttons
+    if verbose:
+        print(f'len(buttons): {len(buttons)}')
+        _ = [print(button.text) for button in buttons]
+    return buttons
+    
+
 def get_bet_buttons(element):
     '''
     for a selenium object, find all of the classes w/ name 'bet-btn'
     '''
-    bet_buttons = element.find_elements_by_class_name(u.BET_BUTTON_CLASS)
+    bet_buttons = element.find_elements_by_class_name(BET_BUTTON_CLASS)
     return bet_buttons
-    
+
+
+def get_bet_buttons_alt(driver=None, verbose=False):
+    '''
+    possibly equivalent to driver.find_elements_by_class_name('bet-btn')
+    see get_bet_buttons in bet_utils.py
+    '''
+    if not driver:
+        driver = get_driver()
+
+    games = get_games(driver, verbose=verbose)
+    mkts = get_mkts(games, verbose=verbose)
+    buttons = buttons_from_mkts(mkts, verbose=verbose)
+    return buttons
+
+
+def locate_btn(game, team_name, mkt_type, verbose=False):
+    '''
+    given selenium game, team_name, and mkt_type
+    returns find the specific bet button for the givens 
+
+    game: selenium obj
+    team_name: str
+    mkt_type: 0 is point spread, 1 is moneyline, and 2 is over/under
+    '''
+    bet_buttons = get_bet_buttons(game)
+    index = btn_index(game, mkt_type, team_name)
+    to_click = bet_buttons[index]
+
+    if verbose:
+        print(f'btn_index: {index}')
+
+    return to_click
+
 
 def to_soup(driver):
     '''
@@ -45,10 +132,16 @@ def to_soup(driver):
     p = bs4.BeautifulSoup(driver.page_source, 'html.parser')
     return p
 
-def get_bet_button(driver=None, team_name='Washington Redskins', amt=20, mkt_type=2, verbose=False):
+
+def find_bet_button(team_name, mkt_type, driver=None, verbose=False):
     gs = get_games(driver)
     g = game_from_team_name(gs, team_name=team_name, verbose=True)
     buttons = get_bet_buttons(g)
+
+
+    index = btn_index(g, mkt_type, team_name)
+    to_click = buttons[index]
+    driver.execute_script("return arguments[0].scrollIntoView();", to_click)
 
     if verbose:
         print(f'game: {g}')
@@ -56,9 +149,6 @@ def get_bet_button(driver=None, team_name='Washington Redskins', amt=20, mkt_typ
         for i, button in enumerate(buttons):
             print(f'button{[i]}: {button.text}')
 
-    index = u.btn_index(g, mkt_type, team_name)
-    to_click = buttons[index]
-    driver.execute_script("return arguments[0].scrollIntoView();", to_click)
     return to_click
 
 
@@ -75,6 +165,20 @@ def game_from_team_name(games, team_name='Washington Redskins', verbose=False):
     if verbose:
         print(f'{team_name} game NOT found')
     return None
+
+
+def team_names_from_games(games, zip_out=False, verbose=False):
+    dogs = []
+    favs = []
+    for game in games:
+        dog, fav = teams_from_game(game, verbose=verbose)
+        dogs.append(dog)
+        favs.append(fav)
+    if zip_out:
+        ret = list(zip(dogs, favs))
+    else:
+        ret = (dogs, favs)
+    return ret
 
 
 def btn_index(game, mkt_type, team_name='Washington Redskins'):
@@ -113,7 +217,7 @@ def trigger_review_slip_alert(driver, buttons=None):
     '''
 
     if not buttons:
-        buttons = get_buttons(driver)
+        buttons = get_bet_buttons_alt(driver)
     print(f'buttons: {buttons}')
     button = buttons[0]
     button.send_keys('\n')
--- sips/lines/bov/better.py
+++ sips/lines/bov/better.py
@@ -9,9 +9,15 @@ from sips.lines.bov import bet_utils as u
 
 
 class Better:
-    def __init__(self):
+    def __init__(self, log_in=False, make_bet=True):
         self.driver = get_driver()
 
+        if log_in:
+            login()
+            
+        if make_bet:
+            bet('Los Angeles Chargers', 1, self.driver)
+            bet('Jacksonville Jaguars', 0, self.driver)
 
 def login(driver=None, captcha_wait=60):
     if not driver:
@@ -41,7 +47,7 @@ def bet(team_name, mkt_type, amt, driver=None, verbose=False):
     ideally input args are (driver, game_id, amt)
     '''
 
-    button = u.get_bet_button(driver, team_name, amt, mkt_type)
+    button = u.find_bet_button(team_name, mkt_type, driver)
     button.send_keys("\n")
     time.sleep(1)
     u.set_wager(driver, 0, amt)
@@ -49,44 +55,7 @@ def bet(team_name, mkt_type, amt, driver=None, verbose=False):
     # if verbose:
     #     print(f'betting: {amt} to win {win_amt}')
 
-    return win_amt
-
-
-def locate_btn(game, team_name, mkt_type, verbose=False):
-    '''
-    given selenium game, team_name, and mkt_type
-    returns find the specific bet button for the givens 
-
-    game: selenium obj
-    team_name: str
-    mkt_type: 0 is point spread, 1 is moneyline, and 2 is over/under
-    '''
-    bet_buttons = get_bet_buttons(game)
-    index = u.btn_index(game, mkt_type, team_name)
-    to_click = bet_buttons[index]
-
-    if verbose:
-        print(f'btn_index: {index}')
-
-    return to_click
-
-
-def bet_on_team(driver, amt, mkt_type=2, team_name='Washington Redskins', verbose=False):
-    '''
-    mkt_type: 0 is point spread, 1 is moneyline, 2 is over under
-    '''
-    games = get_games(driver, verbose=verbose)
-    game = u.game_from_team_name(games, team_name=team_name, verbose=verbose)
-
-    to_click = locate_btn(game, amt=amt, mkt_type=mkt_type,
-                           team_name=team_name, verbose=verbose)
-    # to_click.click()
-    to_click.send_keys("\n")
-    time.sleep(1)
-
-    u.set_wager(driver, 0, amt)
-    return game
-
+    return 
 
 def delete_bets(driver, indices):
     '''
@@ -102,83 +71,6 @@ def delete_bets(driver, indices):
         delete_bet_button.send_keys('\n')
         # delete_bet_button.click()
         print(f'bet[{i}] deleted')
-
-
-def get_games(driver=None, verbose=False):
-    '''
-    uses selenium to find the 'coupon-content'(s)
-    each game has a tag_name of 'section'
-    '''
-    if not driver:
-        driver = get_driver()
-        
-    games = driver.find_elements_by_class_name(u.GAME_CLASS)
-    
-    if verbose:
-        print(f'len(games): {len(games)}')
-        _ = [print(g.text) for g in games]
-
-    return games
-
-
-def team_names_from_games(games, zip_out=False, verbose=False):
-    dogs = []
-    favs = []
-    for game in games:
-        dog, fav = u.teams_from_game(game, verbose=verbose)
-        dogs.append(dog)
-        favs.append(fav)
-    if zip_out:
-        ret = list(zip(dogs, favs))
-    else:
-        ret = (dogs, favs)
-    return ret
-
-
-def get_buttons(driver=None, verbose=False):
-    '''
-
-    '''
-    if not driver:
-        driver = get_driver()
-
-    games = get_games(driver, verbose=verbose)
-    mkts = mkts_from_games(games, verbose=verbose)
-    buttons = buttons_from_mkts(mkts, verbose=verbose)
-    return buttons
-
-
-def mkts_from_game(game, verbose=False):
-
-    game_mkts = game.find_elements_by_class_name(u.MARKETS_CLASS)
-
-    if verbose:
-        _ = [print(mkt.text) for mkt in game_mkts]
-    
-    return game_mkts
-
-
-def mkts_from_games(games, verbose=False):
-    '''
-    returns MARKETS_CLASS class name with selenium
-    '''
-    mkts = []
-
-    for game in games:
-        game_mkts = mkts_from_game(game, verbose=verbose)
-        mkts += game_mkts
-        
-    if verbose:
-        print(f'len(mkts): {len(mkts)}')
-    return mkts
-
-
-def get_bet_buttons(element):
-    '''
-    for a selenium object, find all of the classes w/ name 'bet-btn'
-    '''
-    bet_buttons = element.find_elements_by_class_name(u.BET_BUTTON_CLASS)
-    return bet_buttons
 
 
 def buttons_from_mkts(mkts, verbose=False):
