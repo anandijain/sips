643206f9cca3d6f9eefc3d182d8604bf4b54f437
anandijain <anandj@uchicago.edu>
2019-11-16 13:35:45 -0600

    renamed h.log/ to h.cloud/ and moved gcs.py (which is empty) into it

---
+++ sips/ml/lstm.py
@@ -0,0 +1,9 @@
+'''
+we want a way to easily instantiate a bunch of different models and call them
+functionally and asyncronously.
+
+need to support eval and train modes, where eval doesn't backprop
+
+
+'''
+import torch
