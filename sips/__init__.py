3317b253df3cbd36e40e00bf68b41dbe96a46a4b
anandijain <anandj@uchicago.edu>
2019-12-07 18:35:20 -0600

    relative import error hotfix

--- sips/__init__.py
+++ sips/__init__.py
@@ -8,8 +8,3 @@ sips:
     sportsref/
 
 """
-
-from sips.h.helpers import get_dfs
-from sips.h.serialize import serialize_dfs
-from sips.lines.bov.bov import lines
-from sips.h.grab import page, pages
