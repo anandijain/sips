b673c121899425abfdf9f4b300052a42998b1817
anandijain <anandj@uchicago.edu>
2020-01-16 18:01:44 -0600

    added back in pytorch model for one line games

---
+++ sips/ml/one_line.py
@@ -0,0 +1,137 @@
+import torch
+
+import pandas as pd
+import numpy as np
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset, DataLoader
+
+
+DIR = '/home/sippycups/absa/sips/data/nba/'
+FILES = ['nba_history_with_stats.csv', 'nba_history.csv']
+'/home/sippycups/absa/sips/data/nba/nba_history_with_stats.csv'
+COLS = ['Home_team_gen_avg_3_point_attempt_rate',
+        'Home_team_gen_avg_3_pointers_attempted',
+        'Home_team_gen_avg_3_pointers_made',
+        'Home_team_gen_avg_assist_percentage',
+        'Home_team_gen_avg_assists',
+        'Home_team_gen_avg_block_percentage',
+        'Home_team_gen_avg_blocks',
+        'Home_team_gen_avg_defensive_rating',
+        'Away_team_gen_avg_3_point_attempt_rate',
+        'Away_team_gen_avg_3_pointers_attempted',
+        'Away_team_gen_avg_3_pointers_made',
+        'Away_team_gen_avg_assist_percentage',
+        'Away_team_gen_avg_assists',
+        'Away_team_gen_avg_block_percentage',
+        'Away_team_gen_avg_blocks',
+        'Away_team_gen_avg_defensive_rating',
+        'Away_team_gen_avg_defensive_rebound_percentage',
+        'Away_team_gen_avg_defensive_rebounds']
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(196, 500)
+        self.fc2 = nn.Linear(500, 250)
+        self.fc3 = nn.Linear(250, 125)
+        self.fc4 = nn.Linear(125, 2)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = self.fc4(x)
+        return x
+
+
+class OneLiner(Dataset):
+    """Face Landmarks dataset."""
+
+    def __init__(self, data=None):
+        """
+
+        """
+        if not data:
+            xs, ys = load_data(verbose=True)
+        print(xs)
+        self.xs = torch.tensor(xs.values)
+        # print(self.xs)
+        self.ys = torch.tensor(ys.values, dtype=torch.long)
+        print(self.ys.shape)
+        self.length = len(self.xs)
+        print(self.length)
+
+    def __len__(self):
+        return self.length
+
+    def __getitem__(self, idx):
+        # x = self.xs.iloc[idx].values
+        return {'x': self.xs[idx], 'y': self.ys[idx]}
+
+
+def load_data(batch_size=1, verbose=False):
+    df, df2 = [pd.read_csv(DIR + f) for f in FILES]
+    m = df.merge(df2, left_on='Game_id', right_on='Game_id', how='outer')
+    m = m.fillna(0)
+    # target = pd.get_dummies(m.pop('H_win'))
+    target = m.pop('H_win')
+    m = m.select_dtypes(exclude=['object'])
+    m = m.apply(pd.to_numeric, errors='coerce')
+    # m = m.drop(['A_team_x', 'Game_id', 'H_team_x', 'A_plus_minus',
+                # 'A_team_y', 'Arena', 'H_plus_minus', 'H_team_y', 'Time'], axis=1)
+    # m = m[COLS]
+    target.columns = ['H_win', 'A_win']
+    m = (m-m.mean())/m.std()
+    if verbose:
+        print(f'xs: {m}, target: {target}')
+
+    return m, target
+
+
+if __name__ == "__main__":
+
+    net = Net()
+    dataset = OneLiner()
+
+    for i in range(len(dataset)):
+        sample = dataset[i]
+
+        print(i, sample['x'], sample['y'])
+        if i == 3:
+            x_shape = sample['x'].shape
+            y_shape = sample['y'].shape
+            break
+
+    dataloader = DataLoader(dataset, batch_size=1,
+                            shuffle=True, num_workers=4)
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+    for epoch in range(2):  # loop over the dataset multiple times
+
+        running_loss = 0.0
+        for i, data in enumerate(dataloader, 0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data['x'], data['y']
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = net(inputs.reshape(1, -1).float())
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+            if i % 2000 == 1999:    # print every 2000 mini-batches
+                print('[%d, %5d] loss: %.3f' %
+                      (epoch + 1, i + 1, running_loss / 2000))
+                running_loss = 0.0
+
+    print('Finished Training')
---
+++ sips/ml/one_line_win_loss.py
@@ -0,0 +1,142 @@
+import pandas as pd
+import numpy as np
+
+import tensorflow as tf
+from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras import Model
+from sips.ml import fwd
+
+from sips.ml import utils as tfu
+from sips.ml import lstm
+import random
+from sips.macros import tfm
+
+DIR = '/home/sippycups/absa/sips/data/nba/'
+FILES = ['nba_history_with_stats.csv', 'nba_history.csv']
+'/home/sippycups/absa/sips/data/nba/nba_history_with_stats.csv'
+
+
+class WL(Model):
+  def __init__(self):
+    super(WL, self).__init__()
+
+    self.d1 = Dense(500, activation='relu')
+    self.d2 = Dense(250, activation='relu')
+    self.d3 = Dense(128, activation='relu')
+    self.d4 = Dense(64, activation='relu')
+    self.d5 = Dense(2, activation='softmax')
+
+  def call(self, x):
+    x = self.d1(x)
+    x = self.d2(x)
+    x = self.d3(x)
+    x = self.d4(x)
+    return self.d5(x)
+
+
+def load_data(batch_size=1):
+    # m = m.select_dtypes(exclude=['object'])
+    df, df2 = [pd.read_csv(DIR + f) for f in FILES]
+    m = df.merge(df2, left_on='Game_id', right_on='Game_id', how='outer')
+    m = m.dropna()
+    m = m.apply(pd.to_numeric, errors='coerce')
+    target = pd.get_dummies(m.pop('H_win'))
+    target.columns = ['H_win', 'A_win']
+    m = (m-m.mean())/m.std()
+    data = tf.data.Dataset.from_tensor_slices((m.values, target.values)).batch(batch_size)
+    return data
+
+
+
+# def main():
+    # datasets = [load_data()]
+    # x, y = tfu.get_example(datasets)
+    # loss_fxn = tf.losses.BinaryCrossentropy()
+    # optim = tf.keras.optimizers.Adam()
+    # model = lstm.make_mlp_functional(
+    #     238, 2, classify=True)
+    # log_dir = tfu.get_logdir()
+    # train_step = 0
+
+    # train_summary_writer, test_summary_writer = tfu.init_summary_writers(
+    #     log_dir)
+
+    # (
+    #     train_loss,
+    #     train_accuracy,
+    # ) = tfu.get_classification_metrics()
+
+    # for epoch, dataset in enumerate(datasets):
+    #     if not dataset:
+    #         continue
+    #     for i, (xtr, ytr) in enumerate(dataset):
+    #         ytr = tf.reshape(ytr, (1, -1))
+    #         xtr = tf.reshape(xtr, (1, -1))
+    #         train_loss, train_accuracy, preds = fwd.train_step_classify(
+    #             model, optim, loss_fxn, xtr, ytr, verbose=True
+    #         )
+    #         train_step += 1
+
+    #         maxed_pred = tf.argmax(preds, 1).numpy()[0]
+    #         maxed_true = tf.argmax(ytr).numpy()
+    #         correct = tf.equal(maxed_pred, maxed_true).numpy()
+    #         print(f'step: {i}, loss: {train_loss}, correct: {correct}, preds: {preds}')
+    #     template = "Epoch {}, Loss: {}, Accuracy: {}, Test Loss: {}, Test Accuracy: {}, Preds: {}, Acts: {}"
+    #     print(
+    #         template.format(
+    #             epoch + 1,
+    #             train_loss.result(),
+    #             train_accuracy.result() * 100,
+    #             preds,
+    #             ytr,
+    #         )
+    #     )
+
+    #     # Reset metrics every epoch
+    #     train_loss.reset_states()
+    #     train_accuracy.reset_states()
+
+    #     tf.saved_model.save(model, tfm.WRITE_TO + "win_loss/")
+
+    # return datasets
+
+
+@tf.function
+def train_step(x, y):
+    with tf.GradientTape() as tape:
+        # training=True is only needed if there are layers with different
+        # behavior during training versus inference (e.g. Dropout).
+        predictions = model(x, training=True)
+        loss = loss_object(y, predictions)
+        print(f'{loss},{dir(loss)}')
+        print(predictions)
+        print(y)
+    gradients = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+
+    train_loss(loss)
+    train_accuracy(y, predictions)
+
+
+if __name__ == "__main__":
+    data = load_data()
+    model = WL()
+    loss_object = tf.keras.losses.BinaryCrossentropy()
+    optimizer = tf.keras.optimizers.Adam()
+    train_loss = tf.keras.metrics.Mean(name='train_loss')
+    train_accuracy = tf.keras.metrics.BinaryCrossentropy(
+        name='train_accuracy')
+
+
+    EPOCHS = 1
+
+    for epoch in range(EPOCHS):
+        # Reset the metrics at the start of the next epoch
+        train_loss.reset_states()
+        train_accuracy.reset_states()
+
+        for x, y in data:
+            train_step(x, y)
+
+
+        print(f'{epoch+1}, {train_loss.result()}')
