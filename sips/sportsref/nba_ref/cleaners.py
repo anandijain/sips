aca2ccdbdf181ab551f1f3b2b0a1d5e8c8adeaca
anandijain <anandj@uchicago.edu>
2020-01-24 17:33:54 -0600

    player file cleaning almost ready

--- sips/sportsref/nba_ref/cleaners.py
+++ sips/sportsref/nba_ref/cleaners.py
@@ -32,15 +32,19 @@ def clean_games_files(fns: list, write=False):
             df.to_csv(full_fn)
 
 
-def clean_game_file(fn):
+def clean_game_file(fn:str, verbose=False):
     if "line_score" in fn:
-        df = drop_rename_from_fn(fn, nba.LINE_SCORES)
+        df = drop_rename_from_fn(
+            fn, nba.LINE_SCORES, drop_n=0, verbose=verbose)
     elif "four_factors" in fn:
-        df = drop_rename_from_fn(fn, nba.FOUR_FACTORS)
+        df = drop_rename_from_fn(fn, nba.FOUR_FACTORS,
+                                 drop_n=0, verbose=verbose)
     elif "basic" in fn:
-        df = drop_rename_from_fn(fn, nba.GAME_BASIC)
+        df = drop_rename_from_fn(fn, nba.GAME_BASIC, drop_n=0, verbose=verbose)
     elif "advanced" in fn:
-        df = drop_rename_from_fn(fn, nba.GAME_ADVANCED)
+        df = drop_rename_from_fn(
+            fn, nba.GAME_ADVANCED, drop_n=0, verbose=verbose)
+
     elif "shooting" in fn:
         # drop first col
         df = drop_ith_col(fn, 0)
@@ -130,34 +134,73 @@ def drop_ith_col(fn, i):
     return df
 
 
-def test_player(id=''):
-    for f in TEST_FILES:
+def test_player():
+    for f in PLAYER_TEST_FILES:
         df = clean_player_file(PLAYER_DATA + f, verbose=True)
-        print(f'post: {df}')
+        if df is not None:
+            print(f'post: {df}')
+        else:
+            print(f)
 
 
-TEST_FILES = ["curryst01_advanced.csv",
-              "curryst01_all_salaries.csv",
-              "curryst01_all_college_stats.csv",
-              "curryst01_all_star.csv",
-              "curryst01_pbp.csv",
-              "curryst01_per_game.csv",
-              "curryst01_per_minute.csv",
-              "curryst01_playoffs_advanced.csv",
-              "curryst01_per_poss.csv",
-              "curryst01_playoffs_pbp.csv",
-              "curryst01_playoffs_per_game.csv",
-              "curryst01_playoffs_per_minute.csv",
-              "curryst01_playoffs_per_poss.csv",
-              "curryst01_playoffs_shooting.csv",
-              "curryst01_playoffs_totals.csv",
-              "curryst01_shooting.csv",
-              "curryst01_sim_career.csv",
-              "curryst01_sim_thru.csv",
-              "curryst01_totals.csv",
-              "curryst01_year-and-career-highs-po.csv",
-              "curryst01_year-and-career-highs.csv", ]
+def test_game():
+    for f in GAME_TEST_FILES:
+        df = clean_game_file(GAMES_DATA + f, verbose=True)
+        if df is not None:
+            print(f'post: {df}')
+        else:
+            print(f)
+
+
+
+PLAYER_TEST_FILES = ["curryst01_advanced.csv",
+                     "curryst01_all_salaries.csv",
+                     "curryst01_all_college_stats.csv",
+                     "curryst01_all_star.csv",
+                     "curryst01_pbp.csv",
+                     "curryst01_per_game.csv",
+                     "curryst01_per_minute.csv",
+                     "curryst01_playoffs_advanced.csv",
+                     "curryst01_per_poss.csv",
+                     "curryst01_playoffs_pbp.csv",
+                     "curryst01_playoffs_per_game.csv",
+                     "curryst01_playoffs_per_minute.csv",
+                     "curryst01_playoffs_per_poss.csv",
+                     "curryst01_playoffs_shooting.csv",
+                     "curryst01_playoffs_totals.csv",
+                     "curryst01_shooting.csv",
+                     "curryst01_sim_career.csv",
+                     "curryst01_sim_thru.csv",
+                     "curryst01_totals.csv",
+                     "curryst01_year-and-career-highs-po.csv",
+                     "curryst01_year-and-career-highs.csv", ]
+
+
+GAME_TEST_FILES = ['202001220ORL_box-OKC-game-basic.csv',
+                   '202001220ORL_box-OKC-q2-basic.csv',
+                   '202001220ORL_box-OKC-q3-basic.csv',
+                   '202001220ORL_box-OKC-q4-basic.csv',
+                   '202001220ORL_box-OKC-h2-basic.csv',
+                   '202001220ORL_box-ORL-game-basic.csv',
+                   '202001220ORL_box-ORL-q1-basic.csv',
+                   '202001220ORL_box-ORL-q2-basic.csv',
+                   '202001220ORL_box-ORL-h1-basic.csv',
+                   '202001220ORL_box-ORL-q4-basic.csv',
+                   '202001220ORL_box-ORL-h2-basic.csv',
+                   '202001220ORL_box-ORL-game-advanced.csv',
+                   '202001220ORL_line_score.csv',
+                   '202001220ORL_four_factors.csv',
+                   '202001220ORL_shooting-OKC.csv',
+                   '202001220ORL_shooting-ORL.csv',
+                   '202001220ORL_OKC_shotchart.csv',
+                   '202001220ORL_ORL_shotchart.csv',
+                   '202001220ORL_box-OKC-q1-basic.csv',
+                   '202001220ORL_box-OKC-h1-basic.csv',
+                   '202001220ORL_box-OKC-game-advanced.csv',
+                   '202001220ORL_box-ORL-q3-basic.csv',
+                   '202001220ORL_pbp.csv']
 
 
 if __name__ == "__main__":
-    test_player()
+    # test_player()
+    test_game()
